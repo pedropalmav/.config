ca43204ffa739f3908d6913547aab2e18b2ae31c
pedropalmav <pedro.palma@uc.cl>
2022-08-02 02:02:47 -0400

    Create alias for lsd

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     neofetch
 end
+
+# Aliases
+
+alias ls "lsd"
+
+# Paths
+
 fish_add_path /home/pedropalmav/.spicetify
+
+# Prompt
+
 starship init fish | source
